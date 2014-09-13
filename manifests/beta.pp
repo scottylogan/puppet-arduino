ebb9b4ba0c821504a898833b6fc51d3fc92d233b
Scotty Logan <scotty@catbert.net>
2014-09-13 16:45:00 -0700

    updated beta to latest version; added support for Java 7

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -3,8 +3,14 @@
 # Examples
 #
 #   include arduino::beta
-class arduino::beta inherits arduino {
+class arduino::beta inherits arduino (
+  $java_version = 7,
+) {
+
   Package['Arduino'] {
-    source   => 'http://downloads.arduino.cc/arduino-1.5.6-r2-macosx.zip',
+    source  => $java_version ? {
+      7 => 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip',
+      default => 'http://downloads.arduino.cc/arduino-1.5.7-macosx.zip',
+    }
   }
 }
