8c12caf13ad91a7942e43a5176c8470bbc1ea3d0
Scotty Logan <scotty@catbert.net>
2014-09-13 17:42:38 -0700

    cleaned up versioning support

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -4,13 +4,12 @@
 #
 #   include arduino::beta
 class arduino::beta (
-  $java_version = 7,
-) {
+  $java_version = undef,
+) inherits arduino {
 
   Package['Arduino'] {
-    provider => 'compressed_app',
     source  => $java_version ? {
-      7 => 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip',
+      7       => 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip',
       default => 'http://downloads.arduino.cc/arduino-1.5.7-macosx.zip',
     },
   }
