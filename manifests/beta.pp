b3f76aa193d0b3a34c2b74759b621d86ce0bcc59
Scotty Logan <scotty@catbert.net>
2014-09-13 17:21:55 -0700

    trying without inheritance

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -3,14 +3,15 @@
 # Examples
 #
 #   include arduino::beta
-class arduino::beta inherits arduino (
+class arduino::beta (
   $java_version = 7,
 ) {
 
   Package['Arduino'] {
+    provider => 'compressed_app',
     source  => $java_version ? {
       7 => 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip',
       default => 'http://downloads.arduino.cc/arduino-1.5.7-macosx.zip',
-    }
+    },
   }
 }
