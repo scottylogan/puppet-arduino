6e33c59a170096aed3c30549af72f4dd867e299a
Scotty Logan <swl@stanford.edu>
2015-04-03 11:25:40 -0700

    bumped to 1.6.3, added error for beta

--- manifests/beta.pp
+++ manifests/beta.pp
@@ -7,10 +7,11 @@ class arduino::beta (
   $java_version = undef,
 ) inherits arduino {
 
-  Package['Arduino'] {
-    source  => $java_version ? {
-      7       => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx-java7.zip',
-      default => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx.zip',
-    },
-  }
+#  Package['Arduino'] {
+#    source  => $java_version ? {
+#      7       => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx-java7.zip',
+#      default => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx.zip',
+#    },
+#  }
+  fail('There is no Arduino Beta at this time')
 }
--- spec/classes/arduino__beta_spec.rb
+++ spec/classes/arduino__beta_spec.rb
@@ -2,19 +2,25 @@ require 'spec_helper'
 
 describe "arduino::beta" do
 
-  describe 'when not specifying a java version' do
-    it { should contain_package('Arduino').with({
-      :provider => 'compressed_app',
-      :source => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx.zip',
-    })}
-  end
+#  describe 'when not specifying a java version' do
+#    it { should contain_package('Arduino').with({
+#      :provider => 'compressed_app',
+#      :source => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx.zip',
+#    })}
+#  end
 
-  describe 'when specifying java version 7' do
-    let (:params) {{:java_version => 7}}
-    
-    it { should contain_package('Arduino').with({
-      :provider => 'compressed_app',
-      :source => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx-java7.zip',
-    })}
+#  describe 'when specifying java version 7' do
+#    let (:params) {{:java_version => 7}}
+#    
+#    it { should contain_package('Arduino').with({
+#      :provider => 'compressed_app',
+#      :source => 'http://arduino.cc/download.php?f=/arduino-1.6.1-macosx-java7.zip',
+#    })}
+#  end
+
+  it do
+    expect {
+      should contain_package('Arduino')
+    }.to raise_error(/There is no Arduino Beta at this time/)
   end
 end
