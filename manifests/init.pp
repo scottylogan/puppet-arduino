3f10b94d9196e8b9478202b0c054fb17f0bc49b0
Scotty Logan <swl@stanford.edu>
2015-04-06 11:02:11 -0700

    updated download URL

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class arduino {
   package { 'Arduino':
     provider => 'compressed_app',
-    source   => 'http://arduino.googlecode.com/files/arduino-1.6.3-macosx.zip',
+    source   => 'http://downloads.arduino.cc/arduino-1.6.3-macosx.zip',
   }
 }
